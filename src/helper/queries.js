43d068c829250d3079e2ada8cf992d65b6ac6b02
Fiol Thiago <fiolthiago9@gmail.com>
2024-03-24 10:35:55 -0300

    añadimos todas las funciones para hacer las solicitudes

---
+++ src/helper/queries.js
@@ -0,0 +1,61 @@
+const URL_COLORES = import.meta.env.VITE_API_COLORES;
+
+export const leerColoresAPI = async () => {
+    try {
+      const respuesta = await fetch(`${URL_COLORES}/api/colores`);
+      const recetas = await respuesta.json();
+      return recetas;
+    } catch (error) {
+      console.log(error);
+    }
+}
+
+export const obtenerRecetaAPI = async (id) => {
+    try {
+      const respuesta = await fetch(URL_COLORES + "/api/color/" + id);
+      return respuesta;
+    } catch (error) {
+      console.log(error);
+    }
+};
+
+export const editarColorAPI = async (colorModificado, id) => {
+    try {
+      const respuesta = await fetch(`${URL_COLORES}/api/color/${id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(colorModificado),
+      });
+      return respuesta;
+    } catch (error) {
+      console.log(error);
+    }
+};
+
+export const CrearColorAPI = async (colorNuevo) => {
+  try {
+    const respuesta = await fetch(`${URL_COLORES}/api/colores`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(colorNuevo),
+    });
+    return respuesta;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const borrarColorAPI = async (id) => {
+    try {
+      const respuesta = await fetch(`${URL_COLORES}/api/color/${id}`, {
+        method: "DELETE",
+      });
+      return respuesta;
+    } catch (error) {
+      console.log(error);
+    }
+  };
