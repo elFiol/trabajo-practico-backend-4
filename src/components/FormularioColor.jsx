800b5a83f929c42fccb031b8decefe544f8ef29a
Fiol Thiago <fiolthiago9@gmail.com>
2024-03-24 11:01:33 -0300

    añadios a que funcione la funcion de leer colores , añadimos el input de colores y arreglamos unas cuantas cosas del color

--- src/components/FormularioColor.jsx
+++ src/components/FormularioColor.jsx
@@ -3,20 +3,29 @@ import "./Principal.css"
 import { v4 as uuidv4 } from 'uuid';
 import { useEffect, useState } from "react";
 import ListaColores from "./ListaColores"
+import { leerColoresAPI } from "../helper/queries";
 
 const FormularioColor = () => {
     const [color, setColor] = useState("")
-    const [colores, setColores] = useState(JSON.parse(localStorage.getItem("colorKey")) || [])
+    const [colores, setColores] = useState([])
     const handlerSubmit = (e) => {
         e.preventDefault()
-        setColores([...colores, {id: uuidv4(), nombre: color}])
-        setColor("")
+        setColores([...colores, { id: uuidv4(), nombre: color }])
     }
-    useEffect(()=>{
-        localStorage.setItem("colorKey", JSON.stringify(colores))
-    },[colores])
-    const borrarColor = (id) =>{
-        const coloresFiltrados = colores.filter((color)=> color.id !== id)
+    useEffect(() => {
+        const hacerPeticion = async() => {try{
+            const respuesta = await leerColoresAPI();
+            console.log(respuesta)
+            if(respuesta.length > 0){
+                setColores(respuesta)
+            }}catch(error){
+                console.log(error)
+            }
+        }
+        hacerPeticion()
+    }, [])
+    const borrarColor = (id) => {
+        const coloresFiltrados = colores.filter((color) => color.id !== id)
         setColores(coloresFiltrados)
     }
     return (
@@ -26,9 +35,13 @@ const FormularioColor = () => {
                 <div className="formularioSubDiv">
                     <div className="colorForm"></div>
                     <Form className="w-75" onSubmit={handlerSubmit}>
-                        <Form.Group>
-                            <Form.Control type="text" maxLength={20} minLength={2} placeholder="ingrese un color. EJ: blue" onChange={(e) => { setColor(e.target.value) }} value={color} required></Form.Control>
-                        </Form.Group>
+                        <Form.Label>Ingrese el color</Form.Label>
+                        <Form.Control
+                            type="color"
+                            defaultValue="#0000ff"
+                            title="Choose your color"
+                            onChange={(e)=>setColor(e.target.value)}
+                        />
                         <Button type="submit" className="mt-3">Guardar</Button>
                     </Form>
                 </div>
